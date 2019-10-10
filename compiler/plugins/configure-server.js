423966d7a77a9450b9a2b3dbc7a41a1c0d714f81
Elijah Mooring <ejsisgreat@gmail.com>
2019-10-09 20:44:37 -0500

    Update prettier-options

--- compiler/plugins/configure-server.js
+++ compiler/plugins/configure-server.js
@@ -3,20 +3,20 @@ const read = require('../services/read');
 const { format } = require('prettier');
 const prettierOptions = require('./lib/prettier-options');
 
-module.exports = ({ routes, config }) => {
+module.exports = ({ routes, config, serve }) => {
 	return {
 		name: 'versatile-dynamic',
 		async load(id) {
 			if (/versatile\/startup\/index\.js$/.test(id)) {
 				let code = await read(id);
 
-				const serverData = createServer({ routes, config });
+				const serverData = createServer({ routes, config, serve });
 
 				code = code
 					.replace(`/*{CREATE_SERVER_CODE}*/`, serverData.code)
 					.replace(`/*{IMPORTS_HERE}*/`, `${serverData.imports}`);
 
-				code = format(code, prettierOptions);
+				code = format(code, prettierOptions('babel'));
 				return code;
 			}
 			return null;
--- compiler/plugins/lib/prettier-options.js
+++ compiler/plugins/lib/prettier-options.js
@@ -1,16 +1,18 @@
-module.exports = JSON.parse(`{
-	"arrowParens": "always",
-	"bracketSpacing": true,
-	"endOfLine": "lf",
-	"htmlWhitespaceSensitivity": "css",
-	"insertPragma": false,
-	"printWidth": 120,
-	"proseWrap": "preserve",
-	"requirePragma": false,
-	"semi": true,
-	"singleQuote": true,
-	"tabWidth": 4,
-	"trailingComma": "es5",
-	"useTabs": true,
-	"parser": "babel"
-}`);
+module.exports = (parser) => {
+	return JSON.parse(`{
+		"arrowParens": "always",
+		"bracketSpacing": true,
+		"endOfLine": "lf",
+		"htmlWhitespaceSensitivity": "css",
+		"insertPragma": false,
+		"printWidth": 120,
+		"proseWrap": "preserve",
+		"requirePragma": false,
+		"semi": true,
+		"singleQuote": true,
+		"tabWidth": 4,
+		"trailingComma": "es5",
+		"useTabs": true,
+		"parser": "${parser}"
+	}`);
+};
