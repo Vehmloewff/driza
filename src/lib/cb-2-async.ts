de3ac5553543a07471540c840fdc7ff6d3db2e7f
Elijah <ejsisgreat@gmail.com>
2019-11-26 07:34:40 -0600

    Smash cb-2-async into utils

--- src/lib/cb-2-async.ts
+++
@@ -1,8 +0,0 @@
-export default (fn: Function, ...args: any[]): Promise<any> => {
-	return new Promise((resolve, reject) => {
-		fn(...args, (err: Error, data: any) => {
-			if (err) reject(err);
-			else resolve(data);
-		});
-	});
-};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,12 @@ export const asyncForeach = async (
 		await fn(arr[index], Number(index));
 	}
 };
+
+export const cb2Asnyc = (fn: Function, ...args: any[]): Promise<any> => {
+	return new Promise((resolve, reject) => {
+		fn(...args, (err: Error, data: any) => {
+			if (err) reject(err);
+			else resolve(data);
+		});
+	});
+};
