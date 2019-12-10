7d7eb9e92a1af4faa322f8b03a2ae422afe557a9
Elijah <ejsisgreat@gmail.com>
2019-12-10 09:52:10 -0600

    Create a root

--- renderers/dom/src/index.ts
+++ renderers/dom/src/index.ts
@@ -1,6 +1,24 @@
-import { Mediator } from 'versatilejs';
+import { Mediator, Renderer, createMediator } from 'versatilejs';
+import { Store, simpleStore } from 'versatilejs/store';
 
 export interface RendererResult {
-	data: HTMLElement;
+	element: HTMLElement;
+	removed: Store<boolean>;
 	mediator: Mediator;
 }
+
+const renderer: Renderer<RendererResult> = {
+	root: () => ({
+		element: document.body,
+		mediator: createMediator(),
+		removed: simpleStore(false),
+	}),
+	component: ({ removed }) => ({
+		element: document.createElement('div'),
+		mediator: createMediator(),
+		removed,
+	}),
+	applyFont: async () => {},
+};
+
+export default renderer;
