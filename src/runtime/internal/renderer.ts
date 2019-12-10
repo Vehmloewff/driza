521f6fec7dc7518faa2980106b47c629d019fa77
Elijah <ejsisgreat@gmail.com>
2019-12-10 15:27:27 -0600

    Provide markings for renderer

--- src/runtime/internal/renderer.ts
+++ src/runtime/internal/renderer.ts
@@ -4,13 +4,17 @@ import { Mediator, createMediator } from '../../utils';
 
 export type RendererResult = { data: any; mediator: Mediator };
 
-const renderer: Renderer<RendererResult> = {
-	root: () => ({ data: `some-id`, mediator: createMediator() }),
-	component: ({ type, parent }) => ({
-		data: { element: `${parent}>element:${type}`, id: `some-id` },
-		mediator: createMediator(),
-	}),
-	applyFont: async (font: Font) => console.log('Applying Font...', font),
-};
+const renderer: Renderer<RendererResult> = (function() {
+	/*RENDERER_START*/
+	return {
+		root: () => ({ data: `some-id`, mediator: createMediator() }),
+		component: () => ({
+			data: { element: `parent>element:type`, id: `some-id` },
+			mediator: createMediator(),
+		}),
+		applyFont: async (font: Font) => console.log('Applying Font...', font),
+	};
+	/*RENDERER_END*/
+})();
 
 export default renderer;
