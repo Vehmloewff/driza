5a4a51732f81dde4121c97b2836c37a0c16533f2
Elijah <ejsisgreat@gmail.com>
2019-12-13 21:54:04 -0600

    Add a test

---
+++ src/runtime/style/color.test.ts
@@ -0,0 +1,29 @@
+import { describe } from 'zip-tap';
+import { Color } from './color';
+
+describe(`Color`, it => {
+	it(`should create a color`, expect => {
+		const color = new Color([255, 0, 0]);
+
+		expect(color.hex).toBe(`#ff0000`);
+	});
+
+	it(`should always return lowercase, six or eight hex`, expect => {
+		expect(new Color({ hex: `F0a` }).hex).toBe(`#ff00aa`);
+	});
+
+	it(`should recreate itself when prompted to do so`, expect => {
+		const color = new Color([45, 34, 67]);
+
+		expect(color.hex).toBe(`#2d2243`);
+
+		color.recreate([12, 89, 180]);
+
+		expect(color.hex).toBe(`#0c59b4`);
+	});
+
+	it(`should handle transparency values`, expect => {
+		expect(new Color({ hex: `14fDe32a` }).hex).toBe(`#14fde32a`);
+		expect(new Color({ rgb: [45, 134, 181, 0.7] }).hex).toBe(`#2d86b5b3`);
+	});
+});
