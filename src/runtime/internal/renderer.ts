8785291fa9d2e06ac275d716b663024d05a678ba
Elijah <ejsisgreat@gmail.com>
2019-12-19 15:48:01 -0600

    Add an `onRendererSet` function

--- src/runtime/internal/renderer.ts
+++ src/runtime/internal/renderer.ts
@@ -2,10 +2,20 @@ import { Renderer } from '../interfaces';
 import { Mediator } from '../../utils';
 
 export type RendererResult = { data: any; mediator: Mediator };
+type Callee = (renderer: Renderer<RendererResult>) => void;
 
 let currentRenderer: Renderer<RendererResult> = null;
+const callOnSet: Callee[] = [];
 
-export const setRenderer = (renderer: Renderer<RendererResult>) => (currentRenderer = renderer);
+export const setRenderer = (renderer: Renderer<RendererResult>) => {
+	currentRenderer = renderer;
+
+	callOnSet.forEach(fn => fn(renderer));
+};
+
+export const onRendererSet = (fn: Callee) => {
+	callOnSet.push(fn);
+};
 
 export const getRenderer = () => {
 	if (currentRenderer === null) throw new Error(`Renderer has not been set.  Remember, 'setRenderer' must be called before 'bootstrapComponent'.`);
