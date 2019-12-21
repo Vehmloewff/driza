454d9e5f5c313c7dce6725d955e209f5c87dc7ae
Elijah <ejsisgreat@gmail.com>
2019-12-20 19:14:24 -0600

    Make all UI props optional

--- src/runtime/ui/ui-components.ts
+++ src/runtime/ui/ui-components.ts
@@ -32,32 +32,32 @@ import {
 	ScrollViewResult,
 } from './ui-specs';
 
-export type WebViewComponent = (props: WebViewProps) => ComponentBasics & WebViewResult;
+export type WebViewComponent = (props?: WebViewProps) => ComponentBasics & WebViewResult;
 
-export type HtmlViewComponent = (props: HtmlViewProps) => ComponentBasics & HtmlViewResult;
+export type HtmlViewComponent = (props?: HtmlViewProps) => ComponentBasics & HtmlViewResult;
 
-export type ImageComponent = (props: ImageProps) => ComponentBasics & ImageResult;
+export type ImageComponent = (props?: ImageProps) => ComponentBasics & ImageResult;
 
-export type VideoComponent = (props: VideoProps) => ComponentBasics & VideoResult;
+export type VideoComponent = (props?: VideoProps) => ComponentBasics & VideoResult;
 
-export type AudioComponent = (props: AudioProps) => ComponentBasics & AudioResult;
+export type AudioComponent = (props?: AudioProps) => ComponentBasics & AudioResult;
 
-export type LabelComponent = (props: LabelProps) => ComponentBasics & LabelResult;
+export type LabelComponent = (props?: LabelProps) => ComponentBasics & LabelResult;
 
-export type ElementComponent = (props: ElementProps) => ComponentBasics & ElementResult;
+export type ElementComponent = (props?: ElementProps) => ComponentBasics & ElementResult;
 
-export type TextViewComponent = (props: TextViewProps) => ComponentBasics & TextViewResult;
+export type TextViewComponent = (props?: TextViewProps) => ComponentBasics & TextViewResult;
 
-export type ToggleComponent = (props: ToggleProps) => ComponentBasics & ToggleResult;
+export type ToggleComponent = (props?: ToggleProps) => ComponentBasics & ToggleResult;
 
-export type ChoiceComponent = (props: ChoiceProps) => ComponentBasics & ChoiceResult;
+export type ChoiceComponent = (props?: ChoiceProps) => ComponentBasics & ChoiceResult;
 
-export type AnonymousChoiceComponent = (props: AnonymousChoiceProps) => ComponentBasics & AnonymousChoiceResult;
+export type AnonymousChoiceComponent = (props?: AnonymousChoiceProps) => ComponentBasics & AnonymousChoiceResult;
 
-export type DialogComponent = (props: DialogProps) => ComponentBasics & DialogResult;
+export type DialogComponent = (props?: DialogProps) => ComponentBasics & DialogResult;
 
-export type AbsoluteLayoutComponent = (props: AbsoluteLayoutProps) => ComponentBasics & AbsoluteLayoutResult;
+export type AbsoluteLayoutComponent = (props?: AbsoluteLayoutProps) => ComponentBasics & AbsoluteLayoutResult;
 
-export type StackLayoutComponent = (props: StackLayoutProps) => ComponentBasics & StackLayoutResult;
+export type StackLayoutComponent = (props?: StackLayoutProps) => ComponentBasics & StackLayoutResult;
 
-export type ScrollViewComponent = (props: ScrollViewProps) => ComponentBasics & ScrollViewResult;
+export type ScrollViewComponent = (props?: ScrollViewProps) => ComponentBasics & ScrollViewResult;
