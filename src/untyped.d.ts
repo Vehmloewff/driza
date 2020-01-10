ab5bdd80c283d6ca2550e655b48f5be7ced229bb
Elijah <ejsisgreat@gmail.com>
2020-01-10 10:09:58 -0600

    Remove extra dependencies

--- src/untyped.d.ts
+++ src/untyped.d.ts
@@ -1,5 +1,2 @@
-declare module 'base64-img';
-declare module 'rollup-plugin-node-builtins';
-declare module 'rollup-plugin-node-globals';
 declare module 'make-module';
 declare module 'rollup-plugin-svelte';
